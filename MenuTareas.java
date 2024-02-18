136f8401cdd9e07784028a5ab9d7003c2438f198
unknown <carlocaba2004@gmail.com>
2024-02-18 14:50:45 -0400

    Se añadieron las funciones de añadir marcar como completadas y mostrar reporte de las tareas

--- MenuTareas.java
+++ MenuTareas.java
@@ -1,61 +1,98 @@
 import java.util.*;
 
 public class MenuTareas {
+    static ArrayList<Tarea> listaTareas = new ArrayList<>();
+    
     public static void main(String[] args) {
-        ArrayList <Integer> nLista = new ArrayList();
         Scanner sc = new Scanner(System.in);
-        int i = 0;
-        for(; i != 7; ) {
-            System.out.println("------- MENU ------");
-            System.out.println("1. Agregar Tarea:");
-            System.out.println("2. Marcar Tarea Completada:");
-            System.out.println("3. Eliminar Tarea:");
-            System.out.println("4. Generar reporte general de tareas:");
-            System.out.println("5. Generar reporte Tareas completadas:");
-            System.out.println("6. Generar reporte Tareas pendientes:");
+        int opcion = 0;
+        
+        while (opcion != 7) {
+        	System.out.println("------- MENU  :D ------");
+            System.out.println("1. Agregar Tarea");
+            System.out.println("2. Marcar Tarea Completada");
+            System.out.println("3. Eliminar Tarea");
+            System.out.println("4. Generar reporte general de tareas");
+            System.out.println("5. Generar reporte Tareas completadas");
+            System.out.println("6. Generar reporte Tareas pendientes");
             System.out.println("7. Salir");
-            System.out.println("--------------------------");
             System.out.print("Seleccione una opción: ");
-            i = sc.nextInt();
-            switch(i) {
+            opcion = sc.nextInt();
+            
+            switch (opcion) {
                 case 1:
-                    pausa();
+                    agregarTarea(sc);
                     break;
                 case 2:
-                    pausa();
+                	marcarTareaComoCompletada(sc);
+                    // Marcar Tarea Completada
                     break;
                 case 3:
-                    pausa();
+                    // Eliminar Tarea
                     break;
                 case 4:
-                    pausa();
+                	mostrarListaTareas();
+                    // Generar reporte general de tareas
                     break;
                 case 5:
-                    pausa();
+                    // Generar reporte Tareas completadas
                     break;
                 case 6:
-                    pausa();
+                    // Generar reporte Tareas pendientes
                     break;
                 case 7:
                     System.out.println("Saliendo");
                     break;
                 default:
                     System.out.println("Ingrese una opcion valida");
-                    pausa();
                     break;
             }
         }
         sc.close();
     }
-    
 
-    //void para que la pantalla se muestre el contenido hasta que el usuario quiera cmabiarlo
+
+    static void agregarTarea(Scanner sc) {
+        System.out.println("Ingrese la descripción de la nueva tarea:");
+        sc.nextLine(); // Consumir el salto de línea pendiente
+        String descripcion = sc.nextLine();
+        Tarea nuevaTarea = new Tarea(descripcion);
+        listaTareas.add(nuevaTarea);
+        System.out.println("Tarea agregada exitosamente.");
+        pausa();
+    }
+    
+    static void marcarTareaComoCompletada(Scanner sc) {
+        mostrarListaTareas();
+        System.out.print("Ingrese el número de la tarea que desea marcar como completada: ");
+        int numeroTarea = sc.nextInt();
+        if (numeroTarea >= 1 && numeroTarea <= listaTareas.size()) {
+            Tarea tareaSeleccionada = listaTareas.get(numeroTarea - 1);
+            tareaSeleccionada.marcarComoCompletada();
+            System.out.println("La tarea ha sido marcada como completada.");
+        } else {
+            System.out.println("Número de tarea no válido.");
+        }
+        pausa();
+    }
+
+    
+    static void mostrarListaTareas() {
+        System.out.println("------- Lista de Tareas ------");
+        if (listaTareas.isEmpty()) {
+            System.out.println("No hay tareas guardadas.");
+        } else {
+            for (int i = 0; i < listaTareas.size(); i++) {
+                Tarea tarea = listaTareas.get(i);
+                System.out.println((i + 1) + ". " + tarea.toString());
+            }
+        }
+        pausa();
+    }
+
+
     static void pausa() {
         System.out.println("\nPresione ENTER para continuar...");
         new java.util.Scanner(System.in).nextLine();
     }
-
-
-
 }
-
